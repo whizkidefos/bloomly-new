66f862cfaa9574e7d92241637cd126433ec9f2b1
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-07-01 19:22:20 +0100

    added custom login screen

--- functions.php
+++ functions.php
@@ -181,16 +181,39 @@ function ql_woocommerce_ajax_add_to_cart() {
     $passed_validation = apply_filters('ql_woocommerce_add_to_cart_validation', true, $product_id, $quantity);
     $product_status = get_post_status($product_id); 
     if ($passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status) { 
-        do_action('ql_woocommerce_ajax_added_to_cart', $product_id);
-            if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) { 
-                wc_add_to_cart_message(array($product_id => $quantity), true); 
-            } 
-            WC_AJAX :: get_refreshed_fragments(); 
-            } else { 
-                $data = array( 
-                    'error' => true,
-                    'product_url' => apply_filters('ql_woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id));
-                echo wp_send_json($data);
-            }
-            wp_die();
+    do_action('ql_woocommerce_ajax_added_to_cart', $product_id);
+        if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) { 
+            wc_add_to_cart_message(array($product_id => $quantity), true); 
+        } 
+        WC_AJAX :: get_refreshed_fragments(); 
+        } else { 
+            $data = array( 
+                'error' => true,
+                'product_url' => apply_filters('ql_woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id));
+            echo wp_send_json($data);
         }
+        wp_die();
+}
+
+//Replace style-login.css with the name of your custom CSS file
+function bloomly_custom_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+}
+
+//This loads the function above on the login page
+add_action( 'login_enqueue_scripts', 'bloomly_custom_login_stylesheet' );
+
+function login_error_override()
+{
+    return 'Incorrect login details.';
+}
+add_filter('login_errors', 'login_error_override');
+
+function login_checked_remember_me() {
+    add_filter( 'login_footer', 'rememberme_checked' );
+    }
+    add_action( 'init', 'login_checked_remember_me' );
+    
+    function rememberme_checked() {
+    echo "<script>document.getElementById('rememberme').checked = true;</script>";
+    }

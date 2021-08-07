15f04bf4c7b673e4634f34498059ce0ae25129c2
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-08-07 09:21:56 +0100

    Responsive styling, members dashboard init

--- templates/members-area.php
+++ templates/members-area.php
@@ -9,19 +9,28 @@
         <div class="dashboard-header uk-text-center">
             <h3>Welcome back, Simone! How are you feeling?</h3>
         </div>
-        <ul class="uk-flex-center" uk-tab>
+        <ul class="uk-flex-center" uk-tab uk-switcher="connect: .member-content">
             <li class="uk-active"><a href="#">Your Bloomly Hub</a></li>
             <li><a href="#">Your Test Results</a></li>
             <li><a href="#">Your Order History</a></li>
             <li><a href="#">Scheduled Orders</a></li>
         </ul>
 
-        <ul class="uk-switcher">
-            <li>idiot 1</li>
-            <li>idiot 2</li>
-            <li>idiot 3</li>
-            <li>idiot 4</li>
-        </ul>
+        <div class="uk-child-width-expand@s" uk-grid>
+            <div class="uk-child-width-1-3@m">
+                <?php do_action( 'woocommerce_account_navigation' ); ?>
+            </div>
+            <div class="">
+            <ul class="uk-switcher member-content">
+                <li>
+                    <h5>Your stats</h5>
+                </li>
+                <li>idiot 2</li>
+                <li>idiot 3</li>
+                <li>idiot 4</li>
+            </ul>
+            </div>
+        </div>
     </div>
 </section>
 
